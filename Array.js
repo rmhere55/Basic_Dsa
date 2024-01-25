51e34759c340e26beb23ea6e0431874b2226cb91
raj laxmi singh <112322594+rmhere55@users.noreply.github.com>
2024-01-25 19:20:58 +0000

    traversion and inserten

--- Array.js
+++ Array.js
@@ -1,1 +1,20 @@
+//  threvaseing the array 
+function traversing (){
+    arr = [1,2,3,4,5,6,7]
+    let x = 5;
+    for (let i = 0 ; i< arr.length ; i++){
+        if (arr[i] === x){
+            console.log( i );
+        }
+    }
 
+}
+
+// traversing();
+//    inserting  a array 
+function inserting()
+{
+    let arr = [10,20,30,40,70,80]
+    let newel = 60;
+    
+}
